b43f5888e3032e504806658b90e091f93f0e7a29
Thomas Kinsella <thomasjkinsella4@gmail.com>
2022-04-12 13:58:55 -0400

    commit tree b6173c2f7f14e699ff67e16f5d214dcea1d43204

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,54 @@
+
+
 class Movie < ActiveRecord::Base
+    
+    def self.create_with_title(title)
+        Movie.create(title: title)
+    end
+
+    def self.first_movie
+        Movie.first
+    end
+
+    def self.last_movie
+        Movie.last
+    end
+
+    def self.movie_count
+        Movie.count
+    end
+
+    def self.find_movie_with_id(id)
+        Movie.find(id)
+    end
+
+    def self.find_movie_with_attributes(attribute)
+        Movie.find_by(attribute)
+    end
+
+    def self.find_movies_after_2002
+        # Movie.find_by(Movie.release_date > 2002)
+        Movie.all.select do |movie|
+            movie.release_date > 2002
+        end
+    end
+
+    def update_with_attributes(attr)
+        self.update(attr)
+    end
+
+    def self.update_all_titles(title)
+        Movie.update(title: title)
+    end
+
+    def self.delete_by_id(id)
+        Movie.destroy(id)
+    end
+
+    def self.delete_all_movies
+        Movie.destroy_all
+    end
+
+
 
 end
